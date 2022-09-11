9812851505bc8e77ee5260141f5918e2ef6041fa
Agustín Casado <agu.e.casado@gmail.com>
2022-09-11 17:34:43 -0300

    obtener la geolocalizacion del usuario

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+if ( !navigator.geolocation ) {
+  alert('Navegador no soporta GEOLOCATION');
+  throw new Error('Navegador no soporta GEOLOCATION');
+}
+
 if (environment.production) {
   enableProdMode();
 }
