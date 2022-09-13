a28c4fc34b74415bcc10d47868bc2e2a8e329a20
Agustín Casado <agu.e.casado@gmail.com>
2022-09-13 00:17:49 -0300

    ocultamos el token

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import { mapbox } from './environments/mapboxToken';
  
-Mapboxgl.accessToken = 'pk.eyJ1IjoiYWd1c2NhOTciLCJhIjoiY2w3NnJuYXJoMGF2bDN1cDNwa3N1dnpidSJ9.OWurpI3ZkedBaHP_U58Rfg';
+Mapboxgl.accessToken = mapbox.token;
 
 if ( !navigator.geolocation ) {
   alert('Navegador no soporta GEOLOCATION');
