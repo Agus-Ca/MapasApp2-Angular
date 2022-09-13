36a3b238e0af15699114e00404a3268c59c47696
Agustín Casado <agu.e.casado@gmail.com>
2022-09-12 23:56:03 -0300

    agregamos mapbox a la app

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+Mapboxgl.accessToken = 'pk.eyJ1IjoiYWd1c2NhOTciLCJhIjoiY2w3NnJuYXJoMGF2bDN1cDNwa3N1dnpidSJ9.OWurpI3ZkedBaHP_U58Rfg';
+
 if ( !navigator.geolocation ) {
   alert('Navegador no soporta GEOLOCATION');
   throw new Error('Navegador no soporta GEOLOCATION');
