e9ea3c16208a2919d61633f02544bc26a5c24280
Agustín Casado <agu.e.casado@gmail.com>
2022-09-16 19:30:35 -0300

    dibujamos la poluline

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
+import { AnySourceData, LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
 
 import { DirectionsApiClient } from '../api/directionsApiClient';
 import { Directionresponse, Route } from '../interfaces/directions.interface';
@@ -81,7 +81,44 @@ export class MapService {
 
     console.log( { kms: route.distance / 1000, duration: route.duration / 60 } );
 
-    this.map.fitBounds( bounds, { padding:200 } )
+    this.map.fitBounds( bounds, { padding:200 } );
+
+    const sourceData: AnySourceData = {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: coords
+            }
+          }
+        ]
+      }
+    };
+
+    if ( this.map.getLayer('RouteString')) {
+      this.map.removeLayer('RouteString');
+      this.map.removeSource('RouteString');
+    }
+
+    this.map.addSource('RouteString', sourceData);
+    this.map.addLayer({
+      id: 'RouteString',
+      type: 'line',
+      source: 'RouteString',
+      layout: {
+        'line-cap': 'round',
+        'line-join': 'round'
+      },
+      paint: {
+        'line-color': 'blue',
+        'line-width': 3
+      }
+    });
   }
 
 }
